5b4f4925676546f99493e4d44cfa6c1e1a2bf9c4
Nigmanand_das <81403044+1nigmanand@users.noreply.github.com>
2024-09-19 04:42:06 +0530

    Update bookRoutes.js

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/bookmodel');  // Assumes you have a Book model
-const Review = require('../models/reviewmodel');  // Assumes you have a Review model
+const Book = require('../models/bookModel');  // Assumes you have a Book model
+const Review = require('../models/reviewModel');
 
 // GET /books: Get the list of all books
 router.get('/', async (req, res) => {

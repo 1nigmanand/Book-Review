9d0820fafa71844b3751da1a7484e36f99bf1ab5
Nigmanand_das <81403044+1nigmanand@users.noreply.github.com>
2024-09-19 03:37:01 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+const API_BASE_URL = 'book-review-ivory.vercel.app';
+//http://localhost:5000/api
 // Fetch all books
 export const fetchBooks = async () => {
   try {
